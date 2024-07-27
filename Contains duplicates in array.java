cd09a8b1a3f8f6b5f57bb0695dafa6b3d04ec109
Sruthi <147075888+Sruthi-Ram@users.noreply.github.com>
2024-07-27 14:43:36 +0530

    Create Contains duplicates in array.java

---
+++ Contains duplicates in array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+      Arrays.sort(nums);
+        for(int j=0;j<nums.length-1;j++)
+        {
+            if(nums[j]==nums[j+1])
+            {
+                return true;
+            }
+        }
+       return false;  
+    }
+}
